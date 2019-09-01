a98797f8e35fc955c23b77bd631560cb4f06d5ae
lian0103 <k1319900103@gmail.com>
2019-09-01 19:45:42 +0800

    second

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ function init_snode(){
 }
 
 //加入vue實例方式???
-$('body').before("<script src='components/common/vhead.js'></script>");
-$('body').before("<script src='components/common/vnavMenu.js'></script>");
+$('body').before("<script src='components/v_common.js'></script>");
 $('body').before("<script src='components/product/vproduct.js'></script>");
 
+//加入router
+$('body').before("<script src='router/rindex.js'></script>");
 
-// 組件數的頭 資料的頭???
+// 組件數的頭 資料的頭
 new Vue({
   el: '#webApp_common',
   data: {
@@ -25,11 +26,14 @@ new Vue({
     alermsg:function(){
       alert('work');
     }
-  }
+  },
+  // router:menu_router
 });
 
 //載入後先掛上要呈現的vue實例
 $(function(){
-  new vproduct().$mount('#webApp_content_snode');
+  new vproduct({
+    router
+  }).$mount('#webApp_content_snode');
 })
 
